2678e341a81476402e32a47dfdfebf34e3e64abc
Jing.Dai <daijing_emily@163.com>
2017-09-23 14:56:11 +0800

    add navbar

---
+++ src/js/components/navbar.vue
@@ -0,0 +1,52 @@
+<template>
+    <div class="navbar">
+
+
+        <el-menu :default-active="activeIndex2" class="el-menu-demo" mode="horizontal" @select="handleSelect">
+        <el-row :gutter="20" type="flex" align="middle" justify="space-between">
+          <el-col :span="4">
+            <a href="#" style="float: right;" class="demo-typo-box typo-Microsoft">小打卡</a>
+          </el-col>
+
+        <el-col :span="12">
+          <el-menu-item index="1">小組</el-menu-item>
+          <el-submenu index="2">
+          <template slot="title">我的</template>
+          <el-menu-item index="2-1">我的小組</el-menu-item>
+          <el-menu-item index="2-2">我的記錄</el-menu-item>
+          <el-menu-item index="2-3">我的資料</el-menu-item>
+          </el-submenu>
+        </el-col>
+        <el-col :span="8">
+        <el-menu-item index="3"  style="float: right;">
+          <el-button class="fr mr30 mt15" type="primary" size="small" @click="loginOut">退出登錄</el-button>
+        </el-menu-item>
+        </el-col>
+
+        </el-row>
+        </el-menu>
+    </div>
+</template>
+
+<script>
+    export default{
+        data(){
+            return {
+                name: 'navbar'
+            }
+        },
+        methods: {
+            loginOut(){
+            }
+        },
+        mounted(){
+
+        }
+    }
+</script>
+<style lang="scss" scoped>
+    .navbar {
+        width: 100%;
+        height: 60px;
+    }
+</style>
